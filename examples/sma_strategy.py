4a0f15b3b03e3870d03b012707d675ecf492f147
Kehinde Olalusi <kehindeolalusi75@gmail.com>
2024-05-25 13:12:36 +0100

    Add sma example and update tests.

---
+++ examples/sma_strategy.py
@@ -0,0 +1,60 @@
+from margin_trader.strategy import Strategy
+from margin_trader.data_source import HistoricCSVDataHandler
+from margin_trader.broker import SimBroker
+from margin_trader.trader import Trader
+
+class SMAStrategy(Strategy):
+    window = 3
+
+    def calculate_signals(self, event):
+        if event.type == 'MARKET':
+            for s in self.symbols:
+                bars = self.data.get_latest_bars(s, N=self.window)
+                if bars is not None and len(bars) == self.window:
+                    close_prices = [bar.close for bar in bars]
+                    signal = self._calculate_signal(close_prices)
+                    position = self.broker.get_position(s)
+                    if position:
+                        if signal is not None and position.side != signal:
+                            self.broker.close(s)
+                            if signal == "BUY":
+                                self.broker.buy(s)
+                            else:
+                                self.broker.sell(s)
+                    else:
+                        if signal is not None:
+                            if signal == "BUY":
+                                    self.broker.buy(s)
+                            else:
+                                self.broker.sell(s)
+
+    def _calculate_signal(self, prices: list[float]) -> str:
+        ma = sum(prices) / self.window
+        latest_close = prices[-1]
+        prev_close = prices[-2]
+        if latest_close > ma and prev_close <= ma:
+            return "BUY"
+        elif latest_close < ma and prev_close >= ma:
+            return "SELL"
+        
+if __name__ == "__main__":
+    SYMBOLS = ["AAPL"]
+    DATA_DIR = "./data/"
+
+    data_handler = HistoricCSVDataHandler(
+        csv_dir=DATA_DIR,
+        symbol_list=SYMBOLS
+    )
+    sim_broker = SimBroker(
+        data_handler=data_handler,
+        commission=0.0
+    )
+    strategy = SMAStrategy(symbols=SYMBOLS, data=data_handler, broker=sim_broker)
+    trader = Trader(
+        data_handler=data_handler,
+        broker=sim_broker,
+        strategy=strategy
+    )
+    result = trader._run_backtest()
+    print(result)
+    trader.plot()
---
+++ tests/backtest/test_sma_strategy.py
@@ -0,0 +1,84 @@
+import unittest
+import csv
+import os
+import pandas as pd
+from pathlib import Path
+from examples.sma_strategy import *
+
+CSV_DIR = Path(__file__).parent
+SYMBOLS = ["AAPL"]
+
+class TestTraderBacktest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        data = [
+            ["2024-05-01", 100.0, 105.0, 98.0, 102.0],
+            ["2024-05-02", 102.0, 108.0, 100.0, 106.0],
+            ["2024-05-03", 106.0, 110.0, 104.0, 108.0],  # SMA = 105.33 -> Hold
+            ["2024-05-04", 108.0, 112.0, 106.0, 104.0],  # SMA = 106.0 -> Sell
+            ["2024-05-05", 104.0, 108.0, 102.0, 106.0],  # SMA = 106.0 -> Hold
+            ["2024-05-06", 106.0, 110.0, 104.0, 109.0],  # SMA = 106.33 -> Buy
+            ["2024-05-07", 109.0, 113.0, 107.0, 105.0],  # SMA = 106.67 -> Sell
+            ["2024-05-08", 105.0, 109.0, 103.0, 103.0],  # SMA = 106.0 -> Hold
+            ["2024-05-09", 103.0, 107.0, 101.0, 107.0],  # SMA = 105.0 -> Buy
+            ["2024-05-10", 107.0, 111.0, 105.0, 108.0],  # SMA = 106.67 -> Hold
+        ]
+
+        with open(CSV_DIR/"AAPL.csv", "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                ["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]
+            )
+            for day in data:
+                writer.writerow(day)
+
+        cls.data_handler = HistoricCSVDataHandler(
+            csv_dir=CSV_DIR,
+            symbol_list=SYMBOLS
+        )
+        cls.sim_broker = SimBroker(
+            data_handler=cls.data_handler,
+            commission=0.0
+        )
+        cls.strategy = SMAStrategy(
+            symbols=SYMBOLS,
+            data=cls.data_handler,
+            broker=cls.sim_broker
+        )
+        cls.trader = Trader(
+            data_handler=cls.data_handler,
+            broker=cls.sim_broker,
+            strategy=cls.strategy
+        )
+        cls.result = cls.trader._run_backtest()
+
+    def test_init(self):
+        self.assertTrue(hasattr(self.trader, "events"))
+        self.assertTrue(hasattr(self.trader, "broker"))
+        self.assertTrue(hasattr(self.trader, "data_handler"))
+        self.assertTrue(hasattr(self.trader, "strategy"))
+
+    def test_positions(self):
+        account_history = self.trader.account_history
+        pos_history = account_history["positions"]
+        open_position = self.trader.broker.get_position("AAPL")
+
+        self.assertEqual(len(pos_history), 3)
+        self.assertEqual(pos_history.iloc[0].side, "SELL")
+        self.assertEqual(pos_history.iloc[0].open_time.strftime("%Y-%m-%d"),
+                         "2024-05-04")
+        self.assertEqual(pos_history.iloc[0].close_time.strftime("%Y-%m-%d"),
+                         "2024-05-06")
+        self.assertEqual(pos_history.pnl.sum(), -1100.0)
+        self.assertEqual(open_position.pnl, 100)
+        
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists(CSV_DIR/"AAPL.csv"):
+            os.remove(CSV_DIR/"AAPL.csv")
+
+
+if __name__ == "__main__":
+    unittest.main()
